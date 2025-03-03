8553c8ddf80ef79de9bdd11cdba2ba2bf04a65eb
Parshva Shah <139562275+Parshvashah18@users.noreply.github.com>
2025-03-03 12:08:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        document.querySelector(".hero-content").classList.add("show");
+    }, 2000); // 2-second delay before text fades in
+});
